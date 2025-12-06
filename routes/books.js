c3f73e16a2f2db405bc1e9a1d207c189140ba4bc
hongtrangv <hongtrangv@gmail.com>
2025-12-06 15:04:54 +0000

    .

--- routes/books.js
+++ routes/books.js
@@ -5,14 +5,14 @@ const redisClient = require('../db/redis');
 // Get books from cache
 router.get('/', async (req, res) => {
   try {
-    const cachedBooks = await redisClient.get('cache:book');
+    const cachedBooks = await redisClient.get('cache:books');
     if (cachedBooks) {
       res.send(JSON.parse(cachedBooks));
     } else {
         // In a real application, you would fetch the data from your primary database here
         // and then cache it in Redis.
         const books = [{ id: 1, title: 'Book 1' }]; // placeholder
-        await redisClient.set('cache:book', JSON.stringify(books));
+        await redisClient.set('cache:books', JSON.stringify(books));
         res.send(books);
     }
   } catch (err) {
