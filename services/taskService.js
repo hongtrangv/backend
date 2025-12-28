121162557feaa820358c0e9b889080d06bdc1bc7
hongtrangv <hongtrangv@gmail.com>
2025-12-28 09:26:19 +0000

    .

--- services/taskService.js
+++ services/taskService.js
@@ -15,8 +15,8 @@ const createTaskQuery = async (taskData) => {
   logger.info('Creating task with data:', taskData);
 
   // Extract relevant fields from taskData to ensure no extra data is saved
-  const { description, assignee, isView, date, complete, expried} = taskData;
-  const taskToAdd = { description, assignee, isView, date, complete,expried };
+  const { description, assignee, isView, date, complete, dueDate,createdBy} = taskData;
+  const taskToAdd = { description, assignee, isView, date, complete,dueDate,createdBy };
 
   try {
     const docRef = await db.collection('tasks').add(taskToAdd);
