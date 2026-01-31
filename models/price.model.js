e956f0ac2771bc4d9b1783249052dd57169eb51a
hongtrangv <hongtrangv@gmail.com>
2026-01-31 14:48:08 +0000

    bổ sung object prices

--- models/price.model.js
+++ models/price.model.js
@@ -3,9 +3,9 @@ const mongoose = require('mongoose');
 const priceSchema = new mongoose.Schema({
   productName: { type: String, required: true },
   price: { type: Number, required: true },
-  promotionDate: { type: Date, required: true },
-  quotingUnit: { type: String, required: true },
+  promotionDate: { type: Date, required: true },  
   supplier: { type: String, required: true },
+  notes: { type: String, required: false },
 }, { timestamps: true });
 
 module.exports = mongoose.model('Price', priceSchema);
--- validators/priceValidator.js
+++ validators/priceValidator.js
@@ -3,8 +3,7 @@ const { body, validationResult } = require('express-validator');
 const validatePrice = [
   body('product_name').isString().notEmpty().withMessage('Product name must be a non-empty string'),
   body('price').isFloat({ gt: 0 }).withMessage('Price must be a number greater than 0'),
-  body('effective_date').isISO8601().toDate().withMessage('Invalid promotion date format'),
-  body('quotingUnit').isString().notEmpty().withMessage('Quoting unit must be a non-empty string'),
+  body('effective_date').isISO8601().toDate().withMessage('Invalid promotion date format'),  
   body('supplier').isString().notEmpty().withMessage('Supplier must be a non-empty string'),
   (req, res, next) => {
     const errors = validationResult(req);
